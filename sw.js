a2cdeec42bec52cb197934f006f343c31c2b4847
emscompliance <baydogan.sevtap@gmail.com>
2026-08-14 21:49:32 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
    fully offline.
    ══════════════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'masrawy-v9';
+const CACHE_NAME = 'masrawy-v10';
 const APP_SHELL = [
   './',
   './index.html',
